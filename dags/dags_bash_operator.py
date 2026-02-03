6e1d37f0721ad7da73b2e362fcf979147dd30f39
shchoi <select77@naver.com>
2026-02-03 16:45:11 +0900

    bash_operator dag 업로드8

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import datetime
 import pendulum 
-from airflow.providers.standard.operators.bash import BashOperator
+from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="example_bash_operator",
