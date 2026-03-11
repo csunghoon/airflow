e7c2187e814e100868578ccb6b750a7556983576
shchoi <select77@naver.com>
2026-03-11 12:18:24 +0900

    20260311_03

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -9,30 +9,14 @@ with DAG(
     start_date=pendulum.datetime(2026, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    t1 = EmptyOperator(
-        task_id="t1"
-    )  
-    t2 = EmptyOperator(
-        task_id="t2"
-    )  
-    t3 = EmptyOperator(
-        task_id="t3"
-    )  
-    t4 = EmptyOperator(
-        task_id="t4"
-    )  
-    t5 = EmptyOperator(
-        task_id="t5"
-    )  
-    t6 = EmptyOperator(
-        task_id="t6"
-    )  
-    t7= EmptyOperator(
-        task_id="t7"
-    )  
-    t8= EmptyOperator(
-        task_id="t8"
-    )  
+    t1 = EmptyOperator(task_id="t1")  
+    t2 = EmptyOperator(task_id="t2")  
+    t3 = EmptyOperator(task_id="t3")  
+    t4 = EmptyOperator(task_id="t4")  
+    t5 = EmptyOperator(task_id="t5")  
+    t6 = EmptyOperator(task_id="t6")  
+    t7 = EmptyOperator(task_id="t7")  
+    t8 = EmptyOperator(task_id="t8")  
     
     t1 >> [t2, t3] >> t4
     t5 >> t4
