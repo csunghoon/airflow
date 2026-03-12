f5ae3a800727b76f9a060d1b66f0a755f94fefdf
shchoi <select77@naver.com>
2026-03-12 10:38:38 +0900

    20260312_02

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -12,6 +12,6 @@ with DAG(
     send_email_task = EmailOperator(
        task_id="send_email_task",
        to="select77@naver.com",
-       subject="Airflow 성공메일"
+       subject="Airflow 성공메일",
        html_content="Airflow 작업이 완료되었습니다"
     )  
