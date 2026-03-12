bdded0593d4be0cf13ac1c87436841672b361179
shchoi <select77@naver.com>
2026-03-12 20:04:26 +0900

    20260312_16

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -3,8 +3,8 @@ def get_sftp():
 
 
 def regist(name, sex, *args):
-    print(f'이름: {name}')
-    print(f'성별: {sex}')
-    print(f'기타옵션들: {args}')
+    print(f"이름: {name}")
+    print(f"성별: {sex}")
+    print(f"기타옵션들: {args}")
 
        
