1f9158672ed8ca53420091f779212701fe935ee0
shchoi <select77@naver.com>
2026-03-13 11:41:47 +0900

    20260313_08

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import datatime
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="20 1 * * *",
+    start_date=pendulum.datetime(2026,3,1, tz="Asia/Seoul"),
+    catchup=True,
+) as dag:
+    
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+    
+    show_templates()
